bbde4849dee9d3f52e9fb48bf96e954a6c869301
Иван Агабалаев <agabalaev01@yandex.ru>
2023-01-12 18:50:32 +0300

    Add a viewmodel for the main window
    Bind the title property of the window

---
+++ PCDiagnostics.Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using PCDiagnostics.Client.ViewModels.Base;
+
+namespace PCDiagnostics.Client.ViewModels;
+
+internal class MainWindowViewModel : BaseViewModel
+{
+	private string _title = "PC Diagnostics";
+
+	/// <summary>
+	/// Window title
+	/// </summary>
+	public string Title
+	{
+		get => _title;
+		set => Set(ref _title, value);
+	}
+}
