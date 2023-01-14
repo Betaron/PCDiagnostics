7bbeac5cb62621de8defc0d895d7b1082070ecbf
Иван Агабалаев <agabalaev01@yandex.ru>
2023-01-15 02:05:00 +0300

    Make copy button working
    Copy button copy the nearest pc spec value

--- PCDiagnostics.Client/Views/Tabs/Specs.xaml.cs
+++ PCDiagnostics.Client/Views/Tabs/Specs.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace PCDiagnostics.Client.Views.Tabs
 {
@@ -17,6 +18,11 @@ namespace PCDiagnostics.Client.Views.Tabs
 			ScrollViewer scv = (ScrollViewer)sender;
 			scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta / 7);
 			e.Handled = true;
+		}
+
+		private void Copy_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			Clipboard.SetText((((sender as Button)!.Parent as StackPanel)!.Children[0] as TextBlock)!.Text);
 		}
 	}
 }
