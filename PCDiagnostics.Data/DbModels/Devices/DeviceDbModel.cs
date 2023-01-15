91f329b4dfd6b5af0797f8bfdf79ceb0df6cd16c
Иван Агабалаев <agabalaev01@yandex.ru>
2023-01-15 17:42:35 +0300

    Add Device and Diagnostic database models

---
+++ PCDiagnostics.Data/DbModels/Devices/DeviceDbModel.cs
@@ -0,0 +1,17 @@
+﻿using PCDiagnostics.Core.Domains.Devices;
+
+namespace PCDiagnostics.Data.DbModels.Devices;
+
+public class DeviceDbModel
+{
+	public Guid DiagnosticId { get; set; }
+	public string? Name { get; set; }
+	public Dictionary<string, string>? Specs { get; set; }
+
+	public DeviceDbModel(Device coreDevice)
+	{
+		DiagnosticId = coreDevice.DiagnosticId;
+		Name = coreDevice.Name;
+		Specs = coreDevice.Specs;
+	}
+}
---
+++ PCDiagnostics.Data/DbModels/Diagnostics/DiagnosticDbModel.cs
@@ -0,0 +1,10 @@
+﻿using PCDiagnostics.Data.DbModels.Devices;
+
+namespace PCDiagnostics.Data.DbModels.Diagnostics;
+
+public class DiagnosticDbModel
+{
+	public Guid Id { get; set; }
+	public DateTime CheckTime { get; set; }
+	public List<DeviceDbModel>? Devices { get; set; }
+}
