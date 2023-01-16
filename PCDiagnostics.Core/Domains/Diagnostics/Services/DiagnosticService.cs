d13811a204a6dadb4ac9e7b842c0c67afbddc72d
Иван Агабалаев <agabalaev01@yandex.ru>
2023-01-16 04:05:13 +0300

    Add Diagnostic service class
    Its implements IDiagnosticService.

---
+++ PCDiagnostics.Core/Domains/Diagnostics/Services/DiagnosticService.cs
@@ -0,0 +1,38 @@
+﻿using PCDiagnostics.Core.Domains.Diagnostics.Repositories;
+
+namespace PCDiagnostics.Core.Domains.Diagnostics.Services;
+
+public class DiagnosticService : IDiagnosticService
+{
+	private readonly IDiagnosticRepository _diagnosticRepository;
+
+	public DiagnosticService(IDiagnosticRepository diagnosticRepository)
+	{
+		_diagnosticRepository = diagnosticRepository;
+	}
+
+	public Task CreateAsync(Diagnostic diagnostic, CancellationToken cancellationToken)
+	{
+		return _diagnosticRepository.CreateAsync(diagnostic, cancellationToken);
+	}
+
+	public Task DeleteAsync(Guid id, CancellationToken cancellationToken)
+	{
+		return _diagnosticRepository.DeleteAsync(id, cancellationToken);
+	}
+
+	public Task<IEnumerable<Diagnostic>> GetAllAsync(CancellationToken cancellationToken)
+	{
+		return _diagnosticRepository.GetAllAsync(cancellationToken);
+	}
+
+	public Task<Diagnostic> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+	{
+		return _diagnosticRepository.GetByIdAsync(id, cancellationToken);
+	}
+
+	public Task UpdateAsync(Diagnostic diagnostic, Guid id, CancellationToken cancellationToken)
+	{
+		return _diagnosticRepository.UpdateAsync(diagnostic, id, cancellationToken);
+	}
+}
